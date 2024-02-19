fedf213c2b569b0b1637f9b8b13ffb3e8cd5167c
Brian Grunkemeyer <grunk@live.com>
2024-02-18 18:41:44 -0800

    Fix typo in a method name
    GetAudienceAddressForRegion

--- library/TeslaAuthHelper.cs
+++ library/TeslaAuthHelper.cs
@@ -206,7 +206,7 @@ namespace TeslaAuth
                 ["code_verifier"] = loginInfo.CodeVerifier,
                 ["redirect_uri"] = redirectUri,
                 ["scope"] = scopes,
-                ["audience"] = GetAudienceAdressForRegion(region)
+                ["audience"] = GetAudienceAddressForRegion(region)
         };
 
             using var content = new StringContent(body.ToJsonString(), Encoding.UTF8, "application/json");
@@ -255,7 +255,6 @@ namespace TeslaAuth
 
                 default:
                     throw new NotSupportedException("Region not supported: " + region);
-
             }
         }
 
@@ -264,7 +263,7 @@ namespace TeslaAuth
         /// </summary>
         /// <param name="region">The region that hosts the API that the tokens will be used for</param>
         /// <returns>Address like "https://fleet-api.prd.na.vn.cloud.tesla.com", no trailing slash</returns>
-        static string GetAudienceAdressForRegion(TeslaAccountRegion region)
+        static string GetAudienceAddressForRegion(TeslaAccountRegion region)
         {
             switch (region)
             {
